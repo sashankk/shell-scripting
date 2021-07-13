e2484ab1f9d8793c799231dcde297d66f5b72eec
sashankk <sashank.kalvacharla@gmail.com>
2021-07-13 17:25:59 -0500

    adding printing details

--- 02-print.sh
+++ 02-print.sh
@@ -6,3 +6,18 @@
 
 ##syntax of echo command : echo<Message to print>
 echo Hello World
+
+#syntax for color or new lines : echo -e "Message\n\t\e"
+#echo -e option should need message to be quoted either in single or double quotes
+
+#print multiple lines(\n),some tab space(\t)
+echo Hello World,\n\t Welcome
+
+#enable colors(\e)
+
+#Syntax:echo -e "\e[COLmMESSAGE"
+#colors and there codes are
+#red-31, Green-32, Yellow-33, Blue-34,Magenta-35, Cyan-36
+
+echo -e "\e[31mMessage in RED"
+echo -e "\e[33mMessage in YELLOW"
